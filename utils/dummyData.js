1486a040e3002aefafa802cb75955a3f02e9ea20
VietDinh17 <vietqudinh@gmail.com>
2019-03-30 15:09:31 -0700

    Add test data

---
+++ utils/dummyData.js
@@ -0,0 +1,11 @@
+itemsData = [
+    {
+        "id": "1",
+        "name": "Sandwitch",
+        "description" : "Still eatable",
+        "userID" : "a1",
+        "expireDate": "4/02/2019",
+        "imageURL" : "http://www.paradisepastry.com/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/I/M/IMG_0429__05997.jpg",
+        "location" : "123.41233,-123.4343"
+    }
+]
